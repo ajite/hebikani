94cef0f0df33ecbe6d553a2e8f7c2f23daabde3e
Augustin <ajitekun@gmail.com>
2022-05-30 13:19:06 +0800

    docs: change the doc title name doc to HebiKani

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
-project = "WaniKani CLI"
+project = "HebiKani CLI"
 copyright = "2022, Augustin Cisterne-Kaas"
 author = "Augustin Cisterne-Kaas"
 
